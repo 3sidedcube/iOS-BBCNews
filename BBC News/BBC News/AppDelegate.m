5b59387b10b533e625e2af8d85ae37625d71c4f6
Matthew Cheetham <matt@3sidedcube.com>
2015-03-03 09:56:30 +0000

    Adds top news view as root view of our app so we can see what is going on
    Uh oh, we have a crash because we haven't registered a cell

--- BBC News/BBC News/AppDelegate.m
+++ BBC News/BBC News/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "TSCTopNewsTableViewController.h"
 
 @interface AppDelegate ()
 
@@ -19,6 +20,10 @@
    
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.backgroundColor = [UIColor whiteColor];
+    
+    TSCTopNewsTableViewController *topNews = [[TSCTopNewsTableViewController alloc] init];
+    
+    self.window.rootViewController = topNews;
     
     [self.window makeKeyAndVisible];
     
