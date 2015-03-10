f757071c8f83fc269122aff88a31df769b0b1f66
Simon Mitchell <simon@3sidedcube.com>
2015-03-10 16:29:08 +0000

    Adds model class for a news story

---
+++ BBC News/BBC News/TSCNewsStory.m
@@ -0,0 +1,13 @@
+//
+//  TSCNewsStory.m
+//  BBC News
+//
+//  Created by Simon Mitchell on 10/03/2015.
+//  Copyright (c) 2015 3 SIDED CUBE DESIGN Ltd. All rights reserved.
+//
+
+#import "TSCNewsStory.h"
+
+@implementation TSCNewsStory
+
+@end
