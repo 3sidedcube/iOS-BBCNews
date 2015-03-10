253aafb298d7ab8f240095f02caab564978141fe
Matthew Cheetham <matt@3sidedcube.com>
2015-03-10 11:35:07 +0000

    Adds news detail view

---
+++ BBC News/BBC News/TSCNewsDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  TSCNewsDetailViewController.h
+//  BBC News
+//
+//  Created by Matthew Cheetham on 10/03/2015.
+//  Copyright (c) 2015 3 SIDED CUBE DESIGN Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TSCNewsDetailViewController : UITableViewController
+
+@end
---
+++ BBC News/BBC News/TSCNewsDetailViewController.m
@@ -0,0 +1,24 @@
+//
+//  TSCNewsDetailViewController.m
+//  BBC News
+//
+//  Created by Matthew Cheetham on 10/03/2015.
+//  Copyright (c) 2015 3 SIDED CUBE DESIGN Ltd. All rights reserved.
+//
+
+#import "TSCNewsDetailViewController.h"
+
+@implementation TSCNewsDetailViewController
+
+- (instancetype)init
+{
+    self = [super initWithStyle:UITableViewStyleGrouped];
+    if (self) {
+        
+        
+        
+    }
+    return self;
+}
+
+@end
