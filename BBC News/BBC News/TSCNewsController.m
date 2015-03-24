99e198ccc6e6972216b753cba4f09038ac095644
Matthew Cheetham <matt@3sidedcube.com>
2015-03-24 14:55:04 +0000

    Initialises request controller with Base URL

--- BBC News/BBC News/TSCNewsController.m
+++ BBC News/BBC News/TSCNewsController.m
@@ -11,6 +11,12 @@
 
 static TSCNewsController *sharedController = nil;
 
+@interface TSCNewsController ()
+
+@property (nonatomic, strong) TSCRequestController *requestController;
+
+@end
+
 @implementation TSCNewsController
 
 + (TSCNewsController *)sharedController
@@ -23,6 +29,17 @@ static TSCNewsController *sharedController = nil;
     }
     
     return sharedController;
+}
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        
+        self.requestController = [[TSCRequestController alloc] initWithBaseAddress:@"https://mattcheetham.co.uk/bbc"];
+        
+    }
+    return self;
 }
 
 @end
